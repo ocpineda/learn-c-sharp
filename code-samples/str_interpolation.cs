d75efe87c3afe7567b3de9e4a9230baed7bcd54c
Pineda, Oscar Jr <oscar.pineda@ge.com>
2019-09-30 13:28:31 -0500

    updating str example

--- code-samples/str_interpolation.cs
+++ code-samples/str_interpolation.cs
@@ -10,9 +10,15 @@ public class Program
 		string lastName = "loyno";
 		
 		//This uses composite formatting
-		Console.WriteLine("Hello {0} {1}", firstName, lastName);
+		Console.WriteLine("Hello {0} {1} using composite formatting", firstName, lastName);
 		
 		//This uses string interpolation. Note the dollar sign
 		Console.WriteLine($"Hello {firstName} {lastName}");
+
+		//Different data types. 
+		//item is a named tuple
+		var item = (Name: "eggplant", Price: 1.99m, perPackage: 3);
+		var date = DateTime.Now;
+		Console.WriteLine($"On {date}, the price of {item.Name} was {item.Price:C2} per {item.perPackage} items.");
 	}
 }
